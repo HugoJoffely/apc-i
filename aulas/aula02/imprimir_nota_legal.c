002e3dadb665c24128c211408d9c63dfd87e070b
hugojoffely <hugojcrocha@gmail.com>
2025-03-19 21:51:48 -0300

    Adicionando arquivos da pratica02

---
+++ aulas/aula02/imprimir_nota_legal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    printf("--------------------------------\n");
+    printf("        NOTA LEGAL\n");
+    printf("--------------------------------\n");
+    printf("Item            Qtd  Prc    Valor\n");
+    printf("%-15s %03i %5.2f %7.2f\n", "Caneta Azul", 2, 2.0, 4.0);
+    printf("%-15s %03i %5.2f %7.2f\n", "Borracha", 1, 5.0 ,5.0);
+    printf("%-15s %03i %5.2f %7.2f\n", "Resma de Papel", 1, 10.0, 10.0);
+    printf("---------------------------------\n");
+    printf("               TOTAL: R$ %8.2f\n", 19.00);
+
+    return 0;
+}
---
+++ praticas/pratica02/imprima_seu_nome.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hugo Joffely Cota e Rocha\n");
+
+  return 0;
+}
