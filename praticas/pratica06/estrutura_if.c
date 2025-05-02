3e67c1ecbbbc9fbd78161402bdf57538e4b03147
hugojoffely <hugojcrocha@gmail.com>
2025-05-01 21:06:41 -0300

    pratica06

---
+++ praticas/pratica06/estrutura_if.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int numero;
+    printf("Digite um numero inteiro: ");
+    scanf("%i", &numero);
+int numero_eh_divisivel_por_2 = numero % 2 == 0;
+if (numero_eh_divisivel_por_2) {
+    printf("O numero %i eh par!\n", numero);
+  } else {
+    printf("O numero %i eh impar!\n", numero);
+  }
+  
+    int idade;
+    printf("Digite sua idade: ");
+    scanf("%i", &idade);
+    if (idade < 16) {
+        
+    }
+
+
+  
+
+    return 0;
+}
