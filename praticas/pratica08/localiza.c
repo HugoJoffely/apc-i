3e466bc626655f0623c792f45cea62117ee998f3
Hugo Joffely Cota e Rocha <hugojoffelycotaerocha@MacBook-Air-de-Hugo.local>
2025-05-28 22:02:44 -0300

    pratica08 e aula09

---
+++ praticas/pratica08/localiza.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+
+int numeros [10];
+for(int i=0; i<10; i++){
+    scanf("%i", &numeros[i]);
+}
+
+int numero;
+    scanf("d", &numero);
+
+int achou = -1
+for(int i=0; i<10; i++){
+    if(numeros[i] == numero) {
+        achou = i;
+    }
+}
+
+}
+
+    return 0;
+}
